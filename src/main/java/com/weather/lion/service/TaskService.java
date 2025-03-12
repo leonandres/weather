6f01dd8c0fa22582c809956305a46bfebf8903ba
Andrés León <andresleonb90@gmail.com>
2025-03-12 15:55:41 -0300

    Se agrega @Service en TaskService

--- src/main/java/com/weather/lion/service/TaskService.java
+++ src/main/java/com/weather/lion/service/TaskService.java
@@ -3,9 +3,10 @@ package com.weather.lion.service;
 import com.weather.lion.entity.Task;
 import com.weather.lion.repository.ITaskRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
-
+@Service
 public class TaskService {
     @Autowired
     private ITaskRepository iTaskRepository;
