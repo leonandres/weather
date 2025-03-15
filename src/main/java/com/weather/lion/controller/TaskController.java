cf37f0f6c89fc5d06ed3320c72bfaf06e0455fbe
Andrés León <andresleonb90@gmail.com>
2025-03-15 12:21:06 -0300

    Se agrega metodo a TaskController y WeatherService para llamado a api del clima

--- src/main/java/com/weather/lion/controller/TaskController.java
+++ src/main/java/com/weather/lion/controller/TaskController.java
@@ -2,9 +2,11 @@ package com.weather.lion.controller;
 
 import com.weather.lion.entity.Task;
 import com.weather.lion.service.TaskService;
+import com.weather.lion.service.WeatherService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -15,8 +17,16 @@ public class TaskController {
     @Autowired
     private TaskService taskService;
 
+    @Autowired
+    private WeatherService weatherService;
+
     @GetMapping
     public List<Task> getAllTask(){
         return taskService.getAllTasks();
+    }
+
+    @GetMapping("/weather")
+    public String getWeather(@RequestParam String city){
+        return weatherService.getWeather(city);
     }
 }
--- src/main/java/com/weather/lion/service/WeatherService.java
+++ src/main/java/com/weather/lion/service/WeatherService.java
@@ -1,6 +1,8 @@
 package com.weather.lion.service;
 
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
 @Service
 public class WeatherService {
 
@@ -8,6 +10,8 @@ public class WeatherService {
     private static final String API_URL = "https://api.openweathermap.org/data/2.5/weather";
 
     public String getWeather(String city) {
-        return "";
+        RestTemplate restTemplate = new RestTemplate();
+        String url = API_URL + "?q=" + city + "&appid=" + API_KEY;
+        return restTemplate.getForObject(url, String.class);
     }
 }
