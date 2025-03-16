8db9ba0262d4f918a5c8f77cd6381bc6646ae057
Andrés León <andresleonb90@gmail.com>
2025-03-15 22:17:40 -0300

    Se cambia @RestController por @Controller para visualizar los datos en un html en lugar de ver un json

--- src/main/java/com/weather/lion/controller/TaskController.java
+++ src/main/java/com/weather/lion/controller/TaskController.java
@@ -1,17 +1,21 @@
 package com.weather.lion.controller;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.weather.lion.entity.Task;
 import com.weather.lion.service.TaskService;
 import com.weather.lion.service.WeatherService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@RestController
+@Controller
 @RequestMapping("/tasks")
 public class TaskController {
     @Autowired
@@ -26,7 +30,19 @@ public class TaskController {
     }
 
     @GetMapping("/weather")
-    public String getWeather(@RequestParam String city){
-        return weatherService.getWeather(city);
+    public String getWeather(@RequestParam String city, Model model){
+
+        String response = weatherService.getWeather(city);
+        ObjectMapper mapper = new ObjectMapper();
+        try {
+            JsonNode root = mapper.readTree(response);
+            model.addAttribute("cityName", root.path("name").asText());
+            model.addAttribute("weatherDescription", root.path("weather").get(0).path("description").asText());
+            model.addAttribute("temperature", root.path("main").path("temp").asDouble());
+            return "weather";
+        } catch (Exception e) {
+            model.addAttribute("error", "Error al obtener el clima: " + e.getMessage());
+            return "error";
+        }
     }
 }
