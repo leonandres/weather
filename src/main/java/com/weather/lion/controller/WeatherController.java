1cafd3d64f42a4d9c151b10560f4f720422441ec
Andrés León <andresleonb90@gmail.com>
2025-03-26 01:47:33 -0300

    Se agrega notacion @Controller y @RequestMapping a WeatherController

--- src/main/java/com/weather/lion/controller/WeatherController.java
+++ src/main/java/com/weather/lion/controller/WeatherController.java
@@ -4,10 +4,14 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.weather.lion.service.WeatherService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+@Controller
+@RequestMapping("/weather")
 public class WeatherController {
 
     @Autowired
