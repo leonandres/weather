08a169f8c74335c1cd41ffe7b5531c48885c591a
Andrés León <andresleonb90@gmail.com>
2025-03-26 01:45:13 -0300

    Se agrega método a WeatherController para llamar a api de clima

--- src/main/java/com/weather/lion/controller/WeatherController.java
+++ src/main/java/com/weather/lion/controller/WeatherController.java
@@ -1,7 +1,10 @@
 package com.weather.lion.controller;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.weather.lion.service.WeatherService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -11,7 +14,22 @@ public class WeatherController {
     private WeatherService weatherService;
 
     @GetMapping
-    public String getWeather(){
-        return "";
+    public String getWeather(@RequestParam String city, Model model){
+
+        String response = weatherService.getWeather(city);
+        ObjectMapper mapper = new ObjectMapper();
+        try {
+            JsonNode root = mapper.readTree(response);
+            model.addAttribute("cityName", root.path("name").asText());
+            model.addAttribute("weatherDescription", root.path("weather").get(0).path("description").asText());
+
+            double celsiusTemperature = root.path("main").path("temp").asDouble() - 273.15;
+            String formattedTemperature = String.format("%.1f",celsiusTemperature);
+            model.addAttribute("temperature", formattedTemperature);
+            return "weather";
+        } catch (Exception e) {
+            model.addAttribute("error", "Error al obtener el clima:" + e.getMessage());
+            return "error";
+        }
     }
 }
