32be3273fc12d399ecd722ff0ff990dcbeab6fb9
Andrés León <andresleonb90@gmail.com>
2025-04-02 15:38:44 -0300

    Ajuste estilos

--- src/main/java/com/weather/lion/controller/WeatherController.java
+++ src/main/java/com/weather/lion/controller/WeatherController.java
@@ -24,16 +24,47 @@ public class WeatherController {
         ObjectMapper mapper = new ObjectMapper();
         try {
             JsonNode root = mapper.readTree(response);
+
+            if (root.path("cod").asInt() != 200) {
+                model.addAttribute("error", "Ciudad no encontrada: " + city);
+                return "error";
+            }
+
             model.addAttribute("cityName", root.path("name").asText());
             model.addAttribute("weatherDescription", root.path("weather").get(0).path("description").asText());
 
             double celsiusTemperature = root.path("main").path("temp").asDouble() - 273.15;
             String formattedTemperature = String.format("%.1f",celsiusTemperature);
             model.addAttribute("temperature", formattedTemperature);
+
+            model.addAttribute("humidity", root.path("main").path("humidity").asText() + "%");
+            model.addAttribute("windSpeed", root.path("wind").path("speed").asText() + " km/h");
+
+            String description = root.path("weather").get(0).path("description").asText();
+
+            String weatherIcon = determineWeatherIcon(description);
+            String weatherClass = determineWeatherClass(description);
+
+            model.addAttribute("weatherIcon", weatherIcon);
+            model.addAttribute("weatherClass", weatherClass);
             return "weather";
         } catch (Exception e) {
             model.addAttribute("error", "Error al obtener el clima:" + e.getMessage());
             return "error";
         }
+    }
+
+    private String determineWeatherClass(String description) {
+        String lowerDesc = description.toLowerCase();
+        if (lowerDesc.contains("sol")) return "sunny";
+        if (lowerDesc.contains("lluvia")) return "rainy";
+        return "cloudy";
+    }
+
+    private String determineWeatherIcon(String description) {
+        String lowerDesc = description.toLowerCase();
+        if (lowerDesc.contains("sol")) return "fa-sun";
+        if (lowerDesc.contains("lluvia")) return "fa-cloud-rain";
+        return "fa-cloud";
     }
 }
